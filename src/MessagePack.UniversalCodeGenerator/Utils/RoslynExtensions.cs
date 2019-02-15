ca6480f73743013e2b982aaebf3cb90a17016f05
Andrew Arnott <andrewarnott@gmail.com>
2019-02-15 07:59:11 -0800

    Fix bad merge from much earlier

--- src/MessagePack.UniversalCodeGenerator/Utils/RoslynExtensions.cs
+++ src/MessagePack.UniversalCodeGenerator/Utils/RoslynExtensions.cs
@@ -25,8 +25,6 @@ namespace MessagePack.CodeGenerator
             // https://github.com/daveaglick/Buildalyzer/blob/b42d2e3ba1b3673a8133fb41e72b507b01bce1d6/src/Buildalyzer/Environment/BuildEnvironment.cs#L86-L96
             Dictionary<string, string> properties = new Dictionary<string, string>()
                 {
-                    // trailing '\' may cause unexpected escape
-                    {"IntermediateOutputPath", tempPath + "/"},
                     {"IntermediateOutputPath", tempPath + Path.DirectorySeparatorChar + Path.DirectorySeparatorChar},
                     {"ProviderCommandLineArgs", "true"},
                     {"GenerateResourceMSBuildArchitecture", "CurrentArchitecture"},
